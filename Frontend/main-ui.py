8df1e13a1da005cd04e4eff73636157eb764f33f
Clayton0024 <clayton@pavelkaindustries.com>
2023-06-25 00:04:47 -0500

    Fromatting & Removed unused import statement

--- Frontend/main-ui.py
+++ Frontend/main-ui.py
@@ -1,11 +1,12 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.toolbar import MDTopAppBar
+
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"  # Set the theme to dark
         return Builder.load_file("main-ui.kv")
+
 
 if __name__ == "__main__":
     MainApp().run()
