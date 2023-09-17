0e8085aafdb4a8c68846a8c26e891730ce08db31
Clayton0024 <clayton@pavelkaindustries.com>
2023-09-16 21:33:40 -0500

    added bootstrap for UI

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-# TODO implement startup procedures
-# TODO Start UI
-# TODO Launch Background Process for hardware Fail Safe (likley need multi thread)
+
+import eel
+
+# Set web files folder
+eel.init('./Frontend/web')
+
+@eel.expose                         # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!')   # Call a Javascript function
+
+eel.start('hello.html', size=(300, 200))  # Start
