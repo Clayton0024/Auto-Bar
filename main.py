d6096df8f32aa815e0e6475a06d0267efd6783b3
Clayton0024 <clayton@pavelkaindustries.com>
2023-10-07 21:30:44 -0500

    main ui js

--- main.py
+++ main.py
@@ -4,11 +4,4 @@ import eel
 eel.init("./Frontend/web")
 
 
-@eel.expose  # Expose this function to Javascript
-def say_hello_py(x):
-    print("Hello from %s" % x)
-
-
-eel.say_hello_js("Python World!")  # Call a Javascript function
-
 eel.start("index.html", mode="chrome")
