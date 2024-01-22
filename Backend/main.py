0d49aae92e24b872c51402bc06cf810945bdee35
brha8286 <brha8286@gmail.com>
2024-01-21 22:04:46 -0600

    remove unused things in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,7 @@
 import time
 from autobar_interface import Autobar
-from hardware_interface import HardwareInterface, INTERFACE_ENV
 from communication_interface import TcpCommunicator
 
-# hardware_interface = HardwareInterface("/dev/ttyUSB0", 9600, INTERFACE_ENV.DESKTOP)
 autobar = Autobar()
 
 communicator = TcpCommunicator("127.0.0.1", 6969, autobar.on_message_received)
