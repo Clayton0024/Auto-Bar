9c8529bc19e1b883ad8addfe0e664fa93481dc40
brha8286 <brha8286@gmail.com>
2024-01-29 23:31:06 -0600

    Add logging functionality

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,22 @@
+import datetime
+import logging
 import time
+import subprocess
 from autobar_interface import Autobar
 from communication_interface import TcpCommunicator
+
+# set up logging
+# to use logging in other files, use the following:
+# import logging
+# logging.debug("This is a debug message")
+# logging.info("Informational message")
+# etc.
+now = datetime.datetime.now()
+subprocess.run(['mkdir', '-p', 'logs'])
+log_filename = "autobar_" + now.strftime("%Y%m%d_%H%M%S") + ".log"
+log_filepath = "logs/" + log_filename
+logging.basicConfig(filename=log_filepath, encoding='utf-8', level=logging.DEBUG)
+logging.info("Starting Autobar")
 
 autobar = Autobar()
 
