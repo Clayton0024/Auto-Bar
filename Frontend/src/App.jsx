f15a0d49cf6c4e7f34692f78cb4c68fefcb93fa8
Clayton0024 <clayton@pavelkaindustries.com>
2024-01-31 01:06:54 -0600

    basic app outline and navigation bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NavigationBar } from "./components/navigation-bar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/home";
+import Settings from "./components/pages/settings";
+import { Separator } from "./components/ui/separator";
+import { SearchProvider } from "./searchContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <SearchProvider>
+          <NavigationBar />
+          <Separator className="w-9/10 bg-primary sticky top-[80px]" />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/*" element={<Home />} />
+          </Routes>
+        </SearchProvider>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/components/navigation-bar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { SearchBar } from "./searchBar";
+import { HomeIcon, GearIcon } from "@radix-ui/react-icons";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useSearch } from "../searchContext";
+
+export function NavigationBar() {
+  const locaton = useLocation();
+
+  return (
+    <>
+      <header className="w-full bg-card px-[20px] p-5 flex h-[80px] justify-between items-center sticky top-0">
+        <Link to="/">
+          <HomeIcon className="w-[30px] h-[30px]" />
+        </Link>
+        {locaton.pathname === "/" && <SearchBar />}
+        <Link to="/settings">
+          <GearIcon className="w-[30px] h-[30px]" />
+        </Link>
+      </header>
+    </>
+  );
+}
